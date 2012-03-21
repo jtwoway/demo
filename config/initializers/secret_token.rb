5b659407976152e895af9cfaf2f1bbdf640ce048
jwang <jwang@twowaytv.com>
2012-03-22 10:33:55 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1df74a3e5e934c005c8e260244db0a054af2fecefa5266e018f220fa9950b921d3563bdfe1b2f1f9b8d05103bdb0fd92bfc533163ee99e2ffc74a9ef285e73d7'
